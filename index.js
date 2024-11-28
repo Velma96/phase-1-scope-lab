3ab061ee9a41fc196e24adb2a31d45c9757a24ae
phoebe velma awuor <awuorphoebi@gmail.com>
2024-11-28 11:48:17 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your solution in this file!
+var customerName= "bob";
+function upperCaseCustomerName(bob) {
+    window.customerName = window.customerName.toUpperCase();
+}
+function setBestCustomer() {
+    window.bestCustomer = 'not bob';
+}
+function overwriteBestCustomer(maybeBob) {
+    window.bestCustomer = maybeBob;
+}
+const leastFavoriteCustomer=
+ "anotheCustomer";
+function changeLeastFavoriteCustomer() {
+     leastFavoriteCustomer = "anyOne"
+}
